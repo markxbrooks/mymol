44b8d28e7c497a14b76dfa32f8845655b6d32f41
Mark Brooks <mark.x.brooks@gmail.com>
2023-04-28 14:58:33 -0400

    Create mymol.py

---
+++ mymol.py
@@ -0,0 +1,58 @@
+#!/bin/env python
+from pathlib import Path
+homedir = Path.home()
+
+reinitialize
+cd homedir + os.sep + "Desktop")
+
+prot="1zy7"
+
+print(prot)
+#fetch 1dw1
+cmd.fetch(prot,name=prot)
+
+
+select visible
+#load 1dw1.map.ccp4
+cmd.fetch(prot,type="2fofc")
+
+
+#isomesh map, 1dw1.map, 3.0, sele, carve=1.6
+cmd.isomesh("%s_map"%prot, "%s_2fofc"%prot, 3.0, "sele", carve=1.6)
+
+
+#color br2, map
+cmd.color("br2", "%s_map"%prot)
+#cmd.color("yellow", "%s_map"%prot)
+
+
+#set mesh_width, 0.5
+print cmd.get_setting_text('mesh_width')
+cmd.set("mesh_width", 0.5)
+print cmd.get_setting_text('mesh_width')
+
+
+#bg_color black
+cmd.bg_color("white")
+
+
+#set ray_trace_fog, 0
+print cmd.get_setting_text('ray_trace_fog')
+cmd.set("ray_trace_fog", 0)
+print cmd.get_setting_text('ray_trace_fog')
+
+
+#set depth_cue, 0
+print cmd.get_setting_text('depth_cue')
+cmd.set("depth_cue", 0)
+print cmd.get_setting_text('depth_cue')
+
+
+#set ray_shadows, off
+print cmd.get_setting_text('ray_shadows')
+cmd.set("ray_shadows", "off")
+print cmd.get_setting_text('ray_shadows')
+
+
+#ray 1280,1024
+#cmd.ray(1280,1024)
