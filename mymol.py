3c12e928cda784924a38b24a2be56bed59cc765d
Mark Brooks <Mark.x.brooks@gmail.com>
2023-04-28 22:28:30 -0400

    updating selections

--- mymol.py
+++ mymol.py
@@ -4,7 +4,10 @@ from pathlib import Path
 homedir = Path.home()
 reinitialize
 cd homedir + os.sep + "Desktop"
-prot="1zy7"
+prot="4N1B"
+
+cmpd_resi = 701
+
 chain = A
 print(prot)
 #fetch 1dw1
@@ -28,6 +31,26 @@ cmd.color("br2", "%s_map"%prot)
 print cmd.get_setting_text('mesh_width')
 cmd.set("mesh_width", 0.5)
 print cmd.get_setting_text('mesh_width')
+
+#Compound stuff
+cmd.create("proteinA", "chain A and resi 1:1000")
+cmd.create("cmpd", "chain A and resi 701")
+cmd.create("waters", "resn hoh")
+cmd.create("AS_residues", "byres proteinA within 5.5 of cmpd")
+cmd.show("sticks", "chain A and resi cmpd_name")
+cmd.zoom("cmpd")
+cmd.isomesh("%s_map"%prot, "%s_2fofc"%prot, 1.0, "cmpd", carve=1.6)
+cmd.show("sticks", "cmpd")
+util.cbay organic
+cmd.set("stick_radius", 0.15)
+cmd.color("cyan", "AS_residues")
+
+util.cbao organic
+show cart
+hide line
+show stick, organic or byres(organic expand 4)
+
+
 
 #bg_color black
 cmd.bg_color("white")
